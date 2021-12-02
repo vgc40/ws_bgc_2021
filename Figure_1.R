e4ecfb7186f714f46928ec7ad5c08e0dd4bf58e9
Vanessa Garayburu-Caruso <69696155+vgc40@users.noreply.github.com>
2021-12-02 11:48:38 -0800

    changed the color of the plot

--- Figure_1.R
+++ Figure_1.R
@@ -18,7 +18,7 @@ data = read.csv("Figure_1.csv", skip = 1)
 ggplot(data, aes(x = Year, fill = Study_type)) + 
   coord_cartesian() +  geom_histogram(alpha = 0.5, bins = 50) + 
  geom_vline(aes(xintercept = 2007), colour="black") +
- labs(x = paste0("Year"), y = "Number of papers")+ theme_bw() + theme(text = element_text(size=12, color="black"),axis.text = element_text(color = "black"), axis.ticks = element_line(color = "black"),panel.background = element_blank(), panel.grid = element_blank()) + scale_fill_manual(values = c("blue","green"))
+ labs(x = paste0("Year"), y = "Number of papers")+ theme_bw() + theme(text = element_text(size=12, color="black"),axis.text = element_text(color = "black"), axis.ticks = element_line(color = "black"),panel.background = element_blank(), panel.grid = element_blank()) + scale_fill_manual(values = c("blue","red"))
 
 ggsave(paste0(output.dir,"Figure_1_",Sys.Date(),".pdf"))
 
